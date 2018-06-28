2cc42ef5ab1456b561fddacc6071f34d622c0d60
Rury Chen <Rury.Chen@gmail.com>
2018-06-28 12:44:26 +0800

    Update words.

--- AzureAgentAssignRole.ps1
+++ AzureAgentAssignRole.ps1
@@ -59,23 +59,23 @@ param(
   $AppId
 )
 
-Write-Host  ""
-If([String]::IsNullOrEmpty($AppId)){
-#$AppId = ""
-    Write-Host "The Parameter -AppId cloud not be empty. "
-    Write-Host "SAFE QUIT" -ForegroundColor Green
-    exit
+Write-Host ""
+if ([string]::IsNullOrEmpty($AppId)) {
+  #$AppId = ""
+  Write-Host "The Parameter -AppId cloud not be empty. "
+  Write-Host "SAFE QUIT" -ForegroundColor Green
+  exit
 }
 
 $RequireRoles = (
-    "Reader",
-    "Storage Account Key Operator Service Role"
+  "Reader",
+  "Storage Account Key Operator Service Role"
 )
 
 # Add the Resouce name to here, all resouces start with following name will be ignore
 $SkipResouceNameList = (
-   "xxxx",
-   "subscriptionName2"
+  "xxxx",
+  "subscriptionName2"
 )
 
 function isNotSkip ($name) {
@@ -98,8 +98,8 @@ function ActivateDebug () {
   Write-Host "Debug Enabled writing to logfile: " $LogfileActivated
 }
 
-function WriteDebug($msg) {
-  if(isLog){
+function WriteDebug ($msg) {
+  if (isLog) {
     WriteDebugLogFile $msg
   }
 }
@@ -111,25 +111,25 @@ function WriteDebugLogFile {
   }
 }
 
-function AssignRole ($appId, $subscription,$roleName) {
+function AssignRole ($appId,$subscription,$roleName) {
 
   $scope = "/subscriptions/" + $subscription.SubscriptionId
-  Write-Host (" AssignRole '" + $roleName + "' for subscription: "+ $subscription.Name)
-  WriteDebug (" AssignRole '" + $roleName + "' for subscription: "+ $subscription.Name)
+  Write-Host (" AssignRole '" + $roleName + "' for subscription: " + $subscription.Name)
+  WriteDebug (" AssignRole '" + $roleName + "' for subscription: " + $subscription.Name)
   if ($ProductionRun -eq $true) {
     try {
-         New-AzureRmRoleAssignment -ServicePrincipalName $appId -Scope $scope -RoleDefinitionName $roleName  -ErrorAction Stop > $null
+      New-AzureRmRoleAssignment -ServicePrincipalName $appId -Scope $scope -RoleDefinitionName $roleName -ErrorAction Stop > $null
     }
     catch {
       WriteDebug $_.Exception.Message
       Write-Host ("Failed to assign : " + $_.Exception.Message)
     }
   }
-  
+
 }
 
-function isLog(){
-   return $Log -ne $null -and $Log -eq $true
+function isLog () {
+  return $Log -ne $null -and $Log -eq $true
 }
 Write-Host 'Log' $Log
 if (isLog) {
@@ -160,7 +160,7 @@ Write-Host "This script using for Foglight Azure Agent Role Assign in your subsc
 Write-Host "Require following role: "
 Write-Host ("-" * 90)
 foreach ($temp in $RequireRoles) {
-Write-Host ($temp)
+  Write-Host ($temp)
 
 }
 
@@ -174,11 +174,11 @@ $selectedSubscriptions = New-Object System.Collections.ArrayList
 
 $SkipResource = New-Object System.Collections.ArrayList
 
-Write-Host "Skip add role under following subscriptions. (Edit SkipResouceNameList to add more item) "  -ForegroundColor Yellow
+Write-Host "Skip add role under following subscriptions. (Edit SkipResouceNameList to add more item) " -ForegroundColor Yellow
 Write-Host "----------"
 foreach ($name in $SkipResouceNameList) {
   $SkipResource.Add($name.ToLower()) > $null
-    Write-Host $name.ToLower() -ForegroundColor Red
+  Write-Host $name.ToLower() -ForegroundColor Red
 }
 Write-Host "----------"
 WriteDebug ("Skip add role under following subscriptions. (Edit $SkipResouceNameList to add more item) :" + $SkipResource)
@@ -193,25 +193,25 @@ foreach ($subscription in $Subscriptions) {
   #ask if it should be included
   $title = $subscription.Name
   $id = $subscription.SubscriptionId
-  if($YesToAll){
-      $selectedSubscriptions.Add($subscription) > $null
-      WriteDebug ($id  + " " + $subscription.Name )
-  }else{
-      $message = "Do you want this subscription to be added to the selection?"
-      $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",`
-         "Adds the subscription to the script."
-      $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",`
-         "Skips the subscription from scanning."
-      $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
-      $result = $host.ui.PromptForChoice($title,$message,$options,0)
-      switch ($result) {
-        0 {
-          $selectedSubscriptions.Add($subscription) > $null
-          Write-Host ($subscription.Name + " has been added")
-        }
-        1 { Write-Host ($subscription.Name + " will be skipped")
-        }
+  if ($YesToAll) {
+    $selectedSubscriptions.Add($subscription) > $null
+    WriteDebug ($id + " " + $subscription.Name)
+  } else {
+    $message = "Do you want this subscription to be added to the selection?"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",`
+       "Adds the subscription to the script."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",`
+       "Skips the subscription from scanning."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
+    $result = $host.ui.PromptForChoice($title,$message,$options,0)
+    switch ($result) {
+      0 {
+        $selectedSubscriptions.Add($subscription) > $null
+        Write-Host ($subscription.Name + " has been added")
       }
+      1 { Write-Host ($subscription.Name + " will be skipped")
+      }
+    }
   }
 }
 
@@ -221,10 +221,10 @@ Write-Host "Subscriptions selected:" -ForegroundColor Yellow
 
 foreach ($entry in $selectedSubscriptions) { Write-Host " " + $entry.Name -ForegroundColor Yellow }
 
-    #Press P to continue.
+#Press P to continue.
 if ($ProductionRun -eq $true) {
   Write-Host ""
-  Write-Host "If will do change if you go continue. Set ProductionRun to false just print log. "
+  Write-Host "Set ProductionRun to false just print log. "
   $x = Read-Host 'Press any key to exit or press P to continue.'
   if ($x.ToUpper() -ne "P") {
     Write-Host "SAFE QUIT" -ForegroundColor Green
@@ -233,25 +233,25 @@ if ($ProductionRun -eq $true) {
   #Clear-Host
 }
 
-Write-Host (" Appid Is ( "+ $AppId +" )")
- 
+Write-Host (" Appid Is ( " + $AppId + " )")
+
 #Assign Role
 $i = 1;
 foreach ($subscription in $selectedSubscriptions) {
 
-   Write-Host $i ". Process subscription  " $subscription.Name
-   if(isNotSkip $subscription.Name){
-      foreach ($roleName in $RequireRoles) {
+  Write-Host $i ". Process subscription  " $subscription.Name
+  if (isNotSkip $subscription.Name) {
+    foreach ($roleName in $RequireRoles) {
       AssignRole $AppId $subscription $roleName
-     }
-   }
-   $i++
-   Write-Host ""
+    }
+  }
+  $i++
+  Write-Host ""
 }
 
 Write-Host ""
 if ($ProductionRun -eq $False -or $ProductionRun -eq $null) {
-    Write-Host "Nothing Changes. Please set ProductionRun to $true to make changes. Also You can edit SkipResouceNameList to add Skip Resouce if you want. " -ForegroundColor Cyan 
+  Write-Host "Nothing Changes. Please set ProductionRun to $true to make changes. Also You can edit SkipResouceNameList to add Skip Resouce if you want. " -ForegroundColor Cyan
 }
 Write-Host "Finished."
 
